0e52d47962225ed6e995d88e641df880a941c1e1
sawich <sawich@angeldev.org>
2020-12-09 14:14:16 +0200

    pgsql update item prototype type id

---
+++ SetupDatabase/Migrations/20201209121328_12_09_20_6.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SetupDatabase.Migrations
+{
+    public partial class _12_09_20_6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PrototypeId",
+                table: "items",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "PrototypeId",
+                table: "items",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}
