ee03f15c487296ce882470ee981ee33f03b7b063
sawich <sawich@angeldev.org>
2020-12-09 06:31:02 +0200

    migrations fix

--- SetupDatabase/MigrationContext.cs
+++ SetupDatabase/MigrationContext.cs
@@ -1,26 +1,32 @@
-﻿using ow.Framework.Database;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using ow.Framework.Database;
+using ow.Framework.Database.AccouintPosts;
+using ow.Framework.Database.Accounts;
+using ow.Framework.Database.CharacterPosts;
+using ow.Framework.Database.Characters;
+using ow.Framework.Database.Guilds;
+using ow.Framework.Database.Storages;
 
 namespace Migration.Contexts
 {
-    public sealed class MigrationContext : Context
+    public sealed class MigrationContext : BaseDbContext
     {
-        public DbSet<ow.Framework.Database.Accounts.AccountModel> Account { get; set; }
-        public DbSet<ow.Framework.Database.Characters.CharacterModel> Character { get; set; }
-        public DbSet<ow.Framework.Database.Guilds.GuildModel> Guild { get; set; }
-        public DbSet<ow.Framework.Database.Storages.ItemModel> Item { get; set; }
-        public DbSet<ow.Framework.Database.AccouintPosts.AccountPostModel> AccountPost { get; set; }
-        public DbSet<ow.Framework.Database.CharacterPosts.CharacterPostModel> CharacterPost { get; set; }
+        public DbSet<AccountModel> Account { get; set; }
+        public DbSet<CharacterModel> Character { get; set; }
+        public DbSet<GuildModel> Guild { get; set; }
+        public DbSet<ItemModel> Item { get; set; }
+        public DbSet<AccountPostModel> AccountPost { get; set; }
+        public DbSet<CharacterPostModel> CharacterPost { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder
-                .ApplyConfiguration(new ow.Framework.Database.Accounts.AccountMap())
-                .ApplyConfiguration(new ow.Framework.Database.Characters.CharacterMap())
-                .ApplyConfiguration(new ow.Framework.Database.Guilds.GuildMap())
-                .ApplyConfiguration(new ow.Framework.Database.Storages.ItemMap())
-                .ApplyConfiguration(new ow.Framework.Database.AccouintPosts.AccountPostMap())
-                .ApplyConfiguration(new ow.Framework.Database.CharacterPosts.CharacterPostMap());
+                .ApplyConfiguration(new AccountMap())
+                .ApplyConfiguration(new CharacterMap())
+                .ApplyConfiguration(new GuildMap())
+                .ApplyConfiguration(new ItemMap())
+                .ApplyConfiguration(new AccountPostMap())
+                .ApplyConfiguration(new CharacterPostMap());
         }
     }
 }
