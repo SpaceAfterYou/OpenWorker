b95699b330638d4d262eed712bc67d02322a9d25
sawich <sawich@angeldev.org>
2020-12-14 16:26:43 +0200

    migrations

--- SetupDatabase/Migrations/20201214142616_12_14_20_2.Designer.cs
+++ SetupDatabase/Migrations/20201214142616_12_14_20_2.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SetupDatabase;
@@ -11,9 +12,10 @@ using ow.Framework.Database.Storages;
 namespace SetupDatabase.Migrations
 {
     [DbContext(typeof(MigrationContext))]
-    partial class MigrationContextModelSnapshot : ModelSnapshot
+    [Migration("20201214142616_12_14_20_2")]
+    partial class _12_14_20_2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
