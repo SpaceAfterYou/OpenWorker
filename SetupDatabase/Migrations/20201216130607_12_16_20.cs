1eb9fd28b858997d51cb00b08aa9ac2a5821f91f
sawich <sawich@angeldev.org>
2020-12-16 15:06:24 +0200

    migrations

---
+++ SetupDatabase/Migrations/20201216130607_12_16_20.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SetupDatabase.Migrations
+{
+    public partial class _12_16_20 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Getures",
+                table: "characters",
+                newName: "Gestures");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Gestures",
+                table: "characters",
+                newName: "Getures");
+        }
+    }
+}
