e0f710edbccb8af623c3731c63604f7305ed6f16
sawich <sawich@angeldev.org>
2021-01-04 19:19:00 +0200

    relay service logging handlers

--- ow.RelayService/Worker.cs
+++ ow.RelayService/Worker.cs
@@ -15,11 +15,12 @@ namespace ow.RelayService
 {
     public class Worker : IHostedService
     {
-        private readonly Server _server;
         private readonly ILogger<Worker> _logger;
+        private readonly Server _server;
 
         public Worker(IConfiguration configuration, IServiceProvider services, ILogger<Worker> logger)
         {
+            _logger = logger;
             _server = new()
             {
                 Ports = { CreateServerPort(configuration) }
@@ -27,8 +28,6 @@ namespace ow.RelayService
 
             foreach (ServerServiceDefinition handler in GetHandlers(services))
                 _server.Services.Add(handler);
-
-            _logger = logger;
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
@@ -44,11 +43,23 @@ namespace ow.RelayService
         private static ServerPort CreateServerPort(IConfiguration configuration) =>
             new(configuration["Relay:Host:Ip"], int.Parse(configuration["Relay:Host:Port"]), ServerCredentials.Insecure);
 
-        private static IEnumerable<ServerServiceDefinition> GetHandlers(IServiceProvider services) => AppDomain.CurrentDomain.GetAssemblies()
+        private IEnumerable<ServerServiceDefinition> GetHandlers(IServiceProvider services) => AppDomain.CurrentDomain.GetAssemblies()
             .SelectMany(s => s.GetTypes())
             .Where(s => s.IsDefined(typeof(HandlerAttribute)))
             .Select(s => services.GetRequiredService(s))
-            .Select(s => new { Handler = s, Type = s.GetType() })
+            .Select(s =>
+            {
+                var o = new
+                {
+                    Handler = s,
+                    Type = s.GetType()
+                };
+
+                foreach (MethodInfo method in o.Type.GetMethods(BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Instance).Where(s => s.DeclaringType == o.Type))
+                    _logger.LogDebug($"Used RELAY EVENT ({method.Name}) invoker on {method.DeclaringType!.FullName}.");
+
+                return o;
+            })
             .Select(s => (ServerServiceDefinition)s.Type.GetCustomAttribute<BindServiceMethodAttribute>()!.BindType.GetMethod("BindService", new Type[] { s.Type })?.Invoke(null, new object[] { s.Handler })!);
     }
 }
