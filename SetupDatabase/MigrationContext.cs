114d434c38ca49c924dc79e2088831d1d285ae35
sawich <sawich@angeldev.org>
2021-01-06 13:23:20 +0200

    setup database update

--- SetupDatabase/MigrationContext.cs
+++ SetupDatabase/MigrationContext.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using ow.Framework.Database;
 using ow.Framework.Database.AccouintPosts;
 using ow.Framework.Database.Accounts;
@@ -6,6 +9,7 @@ using ow.Framework.Database.CharacterPosts;
 using ow.Framework.Database.Characters;
 using ow.Framework.Database.Guilds;
 using ow.Framework.Database.Storages;
+using ow.Framework.Extensions;
 
 namespace SetupDatabase
 {
@@ -22,6 +26,10 @@ namespace SetupDatabase
         {
         }
 
+        //public MigrationContext(DbContextOptions options) : base(options)
+        //{
+        //}
+
         protected override void OnModelCreating(ModelBuilder modelBuilder) => modelBuilder
             .ApplyConfiguration(new AccountMap())
             .ApplyConfiguration(new CharacterMap())
@@ -29,5 +37,24 @@ namespace SetupDatabase
             .ApplyConfiguration(new ItemMap())
             .ApplyConfiguration(new AccountPostMap())
             .ApplyConfiguration(new CharacterPostMap());
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args) =>
+            CreateHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .UseConsoleLifetime()
+            .ConfigureAppConfiguration(ConfigureAppConfiguration)
+            .ConfigureServices(ConfigureServices);
+
+        public static void ConfigureAppConfiguration(HostBuilderContext context, IConfigurationBuilder config) => config
+            .AddFrameworkConfig(context);
+
+        public static void ConfigureServices(HostBuilderContext context, IServiceCollection services) => services
+            .AddPooledDbContextFactory<MigrationContext>(c => c
+                .UseNpgsql(context.Configuration.GetConnectionString("PgsqlConnection"), b => b.EnableRetryOnFailure()));
     }
 }
---
+++ SetupDatabase/Migrations/20210106112210_01_06_2021.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SetupDatabase.Migrations
+{
+    public partial class _01_06_2021 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "PrototypeId",
+                table: "items",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PrototypeId",
+                table: "items",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
