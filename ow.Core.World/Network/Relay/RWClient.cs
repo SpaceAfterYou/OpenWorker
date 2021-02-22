a95164c0b94968a8b4a65ae6322bfbd857d74fc8
sawich <sawich@angeldev.org>
2021-02-22 05:19:12 +0200

    move city/maze relary to class project

---
+++ ow.Core.World/Network/Relay/RWClient.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using ow.Framework.IO.Network.Relay;
+using static ow.Framework.IO.Network.Relay.World.Client.Protos.RWCPartyProto;
+using static ow.Framework.IO.Network.Relay.World.Client.Protos.RWCSessionProto;
+
+namespace ow.Core.World.Network.Relay
+{
+    public sealed class RWClient
+    {
+        public readonly RWCPartyProtoClient Party;
+        public readonly RWCSessionProtoClient Session;
+
+        public RWClient(IConfigurationSection configuration)
+        {
+            RChannel channel = new(configuration);
+
+            Party = new(channel);
+            Session = new(channel);
+        }
+    }
+}
