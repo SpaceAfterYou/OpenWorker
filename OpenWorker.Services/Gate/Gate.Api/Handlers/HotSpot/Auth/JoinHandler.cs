0e5134c5ab1bc0e492a244a70aa96f134c74b5d8
sawich <sawich@angeldev.org>
2023-05-23 23:06:39 +0300

    ✨ feat(JoinHandler.cs): add JoinHandler to handle LoginEnterServerMessage and join player to the game server and sync datetime with server

---
+++ OpenWorker.Services/Gate/Gate.Api/Handlers/HotSpot/Auth/JoinHandler.cs
@@ -0,0 +1,31 @@
+﻿using SoulWorkerResearch.SoulCore.IO.Net.Messages.Server.Login;
+using DefaultEcs;
+using OpenWorker.Infrastructure.Communication.HotSpot.Handlers.Abstractions;
+using OpenWorker.Infrastructure.Gameplay.Realm.Components;
+using OpenWorker.Services.Gate.Infrastructure.Gameplay.Abstractions;
+using OpenWorker.Infrastructure.Gameplay.Service.Abstractions;
+
+namespace OpenWorker.Services.Gate.Api.Handlers.HotSpot.Auth;
+
+public sealed record JoinHandler : IHotSpotHandler<LoginEnterServerMessage>
+{
+    private readonly IAuthService _authService;
+    private readonly IServerService _serverService;
+
+    public JoinHandler(IAuthService service, IServerService serverService)
+    {
+        _authService = service;
+        _serverService = serverService;
+    }
+
+    public async ValueTask OnHandleAsync(Entity entity, LoginEnterServerMessage request, CancellationToken ct)
+    {
+        if (!entity.Has<AccountComponent>())
+        {
+            return;
+        }
+
+        await _authService.JoinAsync(request.Account, request.LastGate, request.Key, ct);
+        await _serverService.SyncDateTime(ct);
+    }
+}
