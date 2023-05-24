a3990bf6b0c20d32ded309f8a10d857ea4c1a836
sawich <sawich@angeldev.org>
2023-05-24 15:30:18 +0300

    📦 chore(District.Api.csproj): add HotSpot handlers folders to the project 📦 chore(District.Infrastructure.Gameplay.csproj): add Services/Abstractions folder to the project 🎉 feat(ChannelModel.cs): add ChannelModel class to represent a channel and its entries 🎉 feat(ChannelService.cs): add ChannelService class to handle channel-related operations 🎉 feat(AuthService.cs): add AuthService class to handle authentication-related operations 🎉 feat(MovementService.cs): add MovementService class to handle movement-related operations 🎉 feat(GestureService.cs): add GestureService class to handle gesture-related operations 🎉 feat(ActiveInstance.cs): add ActiveInstance class to represent an active instance with its channels and their online count 🎉 feat(InstanceChannel.cs): add InstanceChannel class to represent a channel with its online count

---
+++ OpenWorker.Services/World/District/District.Infrastructure.Gameplay/Cache/ChannelModel.cs
@@ -0,0 +1,15 @@
+﻿using SoulWorkerResearch.SoulCore.Defines;
+
+namespace OpenWorker.Services.District.Infrastructure.Gameplay.Cache;
+
+public sealed record ChannelModel
+{
+    public ushort District { get; init; }
+    public IEnumerable<Entry> Entries { get; init; } = Enumerable.Empty<Entry>();
+
+    public sealed record Entry
+    {
+        public ushort Id { get; init; }
+        public ChannelLoadStatus Status { get; init; }
+    }
+}
---
+++ OpenWorker.Services/World/District/District.Infrastructure.Gameplay/Services/ChannelService.cs
@@ -0,0 +1,101 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using OpenWorker.Infrastructure.Communication.HotSpot.Session.Abstractions;
+using OpenWorker.Services.District.Infrastructure.Gameplay.Cache;
+using Redis.OM;
+using Redis.OM.Contracts;
+using Redis.OM.Searching;
+using SoulWorkerResearch.SoulCore.IO.Net.Messages.Server.Channel;
+
+namespace OpenWorker.Services.District.Infrastructure.Gameplay.Services;
+
+internal sealed class InstanceChannel
+{
+    public ushort Id { get; init; }
+    public int Online { get; set; }
+}
+
+internal sealed class ActiveInstance
+{
+    internal ushort District { get; }
+    internal IReadOnlyCollection<InstanceChannel> Channels { get; }
+
+    public ActiveInstance(IConfiguration configuration)
+    {
+        District = configuration.GetValue<ushort>("District:Id");
+
+        var min = configuration.GetValue<ushort>("District:MinChannel");
+        var max = configuration.GetValue<ushort>("District:MaxChannel");
+
+        Channels = Enumerable.Range(min, max - min).Select(e => new InstanceChannel { Id = (ushort)e }).ToArray();
+    }
+
+    internal bool InRange(ushort value) => Channels.Any(e => e.Id == value);
+}
+
+internal sealed record GestureService
+{
+    private readonly IHotSpotSession _session;
+
+    public GestureService(IHotSpotSession session) => _session = session;
+}
+
+internal sealed record MovementService
+{
+    private readonly IHotSpotSession _session;
+
+    public MovementService(IHotSpotSession session) => _session = session;
+
+    internal async ValueTask Jump() { }
+    internal async ValueTask LoopMotionEnd() { }
+    internal async ValueTask Move() { }
+    internal async ValueTask StopBroadcast() { }
+}
+
+internal sealed record AuthService
+{
+    private readonly IHotSpotSession _session;
+
+    public AuthService(IHotSpotSession session) => _session = session;
+
+    internal async ValueTask Join()
+    {
+
+    }
+}
+
+internal sealed record ChannelService
+{
+    private readonly IHotSpotSession _session;
+    private readonly ActiveInstance _instance;
+    private readonly IRedisCollection<ChannelModel> _channels;
+
+    public ChannelService(IHotSpotSession session, ActiveInstance channels, IRedisConnectionProvider provider)
+    {
+        _session = session;
+        _instance = channels;
+        _channels = provider.RedisCollection<ChannelModel>();
+    }
+
+    internal async ValueTask ShowList(CancellationToken ct = default)
+    {
+        var entries = _channels.Where(e => e.District == _instance.District).SelectMany(e => e.Entries);
+
+        await _session.SendAsync(new ChannelInfoClientMessage
+        {
+            Map = _instance.District,
+            Values = entries.Select(e => new ChannelInfoClientMessage.Entry { Id = e.Id, Status = e.Status, })
+        }, ct);
+    }
+
+    internal async ValueTask Switch(ushort id)
+    {
+        if (_instance.InRange(id))
+        {
+            _session.Entity.Set<InstanceChannel>();
+
+            // await _session.SendAsync(new ChannelChangeClientMessage() { Channel = id });
+            return;
+        }
+    }
+}
